d0933af1287d18ac3fda71df74b1a628811b8340
MoreKayaa <mokayafavian@gmail.com>
2025-04-21 20:37:47 +0300

    modify the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,22 @@
-import React from "react";
-import Layout from "../components/layout/Layout";
-import HeroSection from "../components/sections/HeroSection";
-import FeaturesSection from "../components/sections/FeaturesSection";
-import TeamSection from "../components/sections/TeamSection";
-import MethodologySection from "../components/sections/MethodologySection";
-import TechnologySection from "../components/sections/TechnologySection";
-import CTASection from "../components/sections/CTASection";
+import React from 'react';
+import Navbar from '../components/layout/Navbar';
+import Footer from '../components/layout/Footer';
+import HeroSection from '../components/sections/HeroSection';
+import FeaturesSection from '../components/sections/FeaturesSection';
+import HowItWorksSection from '../components/sections/HowItWorksSection';
 
 const HomePage: React.FC = () => {
   return (
-    <Layout>
-      <HeroSection />
-      <FeaturesSection />
-      <TechnologySection />
-      <MethodologySection />
-      <TeamSection />
-      <CTASection />
-    </Layout>
+    <>
+      <Navbar />
+      <main>
+        <HeroSection />
+        <FeaturesSection />
+        <HowItWorksSection />
+        {/* Add additional sections here */}
+      </main>
+      <Footer />
+    </>
   );
 };
 
