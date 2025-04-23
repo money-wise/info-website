521c953db144a1a97020d52fd73afcb79fe68299
WaruiAlfred <alfredwarui17@gmail.com>
2025-04-23 14:53:53 +0300

    refactor: remove unnecessary Router component wrapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import "./App.css";
 
 function App() {
   return (
-    // <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
       {/* Add more routes as needed */}
     </Routes>
-    // </Router>
   );
 }
 
-export default App
+export default App;
